9a2e44062fb101f042ac1226ce5a70bae241ccc5
Aaron Diaz <90427861+N0-R4NS0M@users.noreply.github.com>
2025-07-02 16:56:09 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,56 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "threat-detection-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "${var.aws_region}a"
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "threat_target" {
+  ami           = var.ec2_ami
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name      = var.ssh_key_name
+
+  tags = {
+    Name = "threat-target"
+  }
+}
+
+resource "aws_guardduty_detector" "main" {
+  enable = true
+}
