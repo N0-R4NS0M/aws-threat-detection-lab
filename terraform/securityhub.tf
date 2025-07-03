38f4e343ddff52ccb2c8c1ffa75bf1001d8d6c12
Aaron Diaz <90427861+N0-R4NS0M@users.noreply.github.com>
2025-07-03 16:02:11 -0700

    Update securityhub.tf

--- terraform/securityhub.tf
+++ terraform/securityhub.tf
@@ -1,6 +1,11 @@
-resource "aws_securityhub_account" "main" {}
+resource "aws_securityhub_account" "main" {
+  # This enables Security Hub for the account
+}
 
 resource "aws_securityhub_standards_subscription" "cis_aws_foundations" {
   standards_arn = "arn:aws:securityhub:::standards/cis-aws-foundations-benchmark/v/1.2.0"
-  depends_on = [aws_securityhub_account.main]
+
+  depends_on = [
+    aws_securityhub_account.main
+  ]
 }
