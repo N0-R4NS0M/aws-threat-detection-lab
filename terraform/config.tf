7d1392b9c61faa8306f84e8801c6a392919614a6
Aaron Diaz <90427861+N0-R4NS0M@users.noreply.github.com>
2025-07-03 15:41:33 -0700

    Update config.tf

--- terraform/config.tf
+++ terraform/config.tf
@@ -15,6 +15,35 @@ resource "aws_s3_bucket" "config_logs" {
   bucket = "config-logs-${random_id.bucket_suffix.hex}"
 }
 
+resource "aws_s3_bucket_policy" "config_logs_policy" {
+  bucket = aws_s3_bucket.config_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSConfigBucketPermissionsCheck",
+        Effect    = "Allow",
+        Principal = { Service = "config.amazonaws.com" },
+        Action    = "s3:GetBucketAcl",
+        Resource  = aws_s3_bucket.config_logs.arn
+      },
+      {
+        Sid       = "AWSConfigBucketDelivery",
+        Effect    = "Allow",
+        Principal = { Service = "config.amazonaws.com" },
+        Action    = "s3:PutObject",
+        Resource  = "${aws_s3_bucket.config_logs.arn}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
