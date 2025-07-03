fb7a916584ecaf86a869a385d330563b65db256d
Aaron Diaz <90427861+N0-R4NS0M@users.noreply.github.com>
2025-07-03 16:41:31 -0700

    Update securityhub.tf

--- terraform/securityhub.tf
+++ terraform/securityhub.tf
@@ -3,7 +3,7 @@ resource "aws_securityhub_account" "main" {
 }
 
 resource "aws_securityhub_standards_subscription" "cis_aws_foundations" {
-  standards_arn = "arn:aws:securityhub:::standards/cis-aws-foundations-benchmark/v/1.2.0"
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 
   depends_on = [
     aws_securityhub_account.main
